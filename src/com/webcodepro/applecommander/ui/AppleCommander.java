c37e5767a227238773db29bc3f4a7d0f9aa09702
Robert Greene <robgreene@users.sourceforge.net>
2003-12-06 04:14:00 +0000

    Updated year in comments.

--- src/com/webcodepro/applecommander/ui/AppleCommander.java
+++ src/com/webcodepro/applecommander/ui/AppleCommander.java
@@ -1,6 +1,6 @@
 /*
  * AppleCommander - An Apple ][ image utility.
- * Copyright (C) 2002 by Robert Greene
+ * Copyright (C) 2002-3 by Robert Greene
  * robgreene at users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or modify it 
