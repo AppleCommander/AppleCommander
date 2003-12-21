9a5d208c197e08a4c98dfcd351cfe6e77d61df97
Robert Greene <robgreene@users.sourceforge.net>
2003-12-21 06:16:58 +0000

    1.3.2pre

--- src/com/webcodepro/applecommander/ui/AppleCommander.java
+++ src/com/webcodepro/applecommander/ui/AppleCommander.java
@@ -32,14 +32,14 @@ import java.lang.reflect.Method;
  * Regarding SWT, this appliation launcher tries to not be SWT dependent.
  * That means that SwtAppleCommander is launched purely by reflection.
  * NOTE: This may yet prove to be a worthless trick.  If it is, remove
- * the crud.  However, as the VERION and COPYRIGHT are in this class and
+ * the crud.  However, as the VERSION and COPYRIGHT are in this class and
  * are referenced in various places, it may well be worth it.
  * <p>
  * Date created: Nov 16, 2002 9:13:25 PM
  * @author: Rob Greene
  */
 public class AppleCommander {
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2pre";
 	public static final String COPYRIGHT = "Copyright (c) 2002-2003";
 	/**
 	 * Launch AppleCommander.
