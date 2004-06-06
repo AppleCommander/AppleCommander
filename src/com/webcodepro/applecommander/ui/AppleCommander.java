8b58cc3f24e20ac0bf39e477dc08763c1a653e75
Robert Greene <robgreene@users.sourceforge.net>
2004-06-06 02:14:23 +0000

    Updated copyright and version number.

--- src/com/webcodepro/applecommander/ui/AppleCommander.java
+++ src/com/webcodepro/applecommander/ui/AppleCommander.java
@@ -39,8 +39,8 @@ import java.lang.reflect.Method;
  * @author: Rob Greene
  */
 public class AppleCommander {
-	public static final String VERSION = "1.3.2pre";
-	public static final String COPYRIGHT = "Copyright (c) 2002-2003";
+	public static final String VERSION = "1.3.2";
+	public static final String COPYRIGHT = "Copyright (c) 2002-2004";
 	/**
 	 * Launch AppleCommander.
 	 */
