680f0e57328ea194d77fb4d722890e37b563db23
Robert Greene <robgreene@users.sourceforge.net>
2004-06-18 02:14:48 +0000

    Simple ContentTypeAdapter to do no action.

---
+++ src/com/webcodepro/applecommander/ui/swt/util/contentadapter/NoActionContentTypeAdapter.java
@@ -0,0 +1,26 @@
+/*
+ * Created on Jun 17, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.webcodepro.applecommander.ui.swt.util.contentadapter;
+
+/**
+ * A content adapter that implements all methods but performs no actions.
+ * 
+ * @author Rob Greene
+ */
+public class NoActionContentTypeAdapter implements ContentTypeAdapter {
+	public void copy() {
+		// Do Nothing!!
+	}
+
+	public void print() {
+		// Do Nothing!!
+	}
+
+	public void selectAll() {
+		// Do Nothing!!
+	}
+}
