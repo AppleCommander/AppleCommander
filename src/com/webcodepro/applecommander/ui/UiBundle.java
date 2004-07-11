c4cd2c699bdc570651a1915c90f6015ae493a086
Robert Greene <robgreene@users.sourceforge.net>
2004-07-11 15:08:10 +0000

    Made TextBundle a general utility class and changed to the UI-specific UiBundle.

---
+++ src/com/webcodepro/applecommander/ui/UiBundle.java
@@ -0,0 +1,43 @@
+/*
+ * AppleCommander - An Apple ][ image utility.
+ * Copyright (C) 2002-2004 by Robert Greene
+ * robgreene at users.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation; either version 2 of the License, or (at your 
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package com.webcodepro.applecommander.ui;
+
+import com.webcodepro.applecommander.util.TextBundle;
+
+/**
+ * Manage the user-interface specific ResourceBundle.  Inheritance is used to
+ * allow logical groupings of ResourceBundles and to reuse the common metods.
+ * 
+ * @author Rob Greene
+ */
+public class UiBundle extends TextBundle {
+	private static UiBundle instance;
+	
+	/**
+	 * Get the singleton instance of the UiBundle. 
+	 */
+	public static TextBundle getInstance() {
+		if (instance == null) {
+			instance = new UiBundle();
+			instance.initialize();
+		}
+		return instance;
+	}
+}
