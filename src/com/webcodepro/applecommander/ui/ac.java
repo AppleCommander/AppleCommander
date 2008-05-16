111502da4bf62a69ef1cd7d31be9dd9aff092876
John B. Matthews <matthewsj@users.sourceforge.net>
2008-05-16 21:23:19 +0000

    Update developer contact information.

--- src/com/webcodepro/applecommander/ui/ac.java
+++ src/com/webcodepro/applecommander/ui/ac.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2002 by Robert Greene
  * robgreene at users.sourceforge.net
  * Copyright (C) 2003, 2004 by John B.  Matthews
- * jmatthews at wight dot edu
+ * matthewsj at users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the 
