AppleCommander/AppleCommander

commit 292afd24c93ce440ecff3272ce11ce9d162dfece

Remove -Xlint:unchecked warnings
Fixed all instances of warnings OpenJDK finds with -Xlint:unchecked.
Merging this will at least result in a dependency on JDK 1.5 or later
that I'm pretty sure of.  But since the only JDKs that are really safe
to use these days are versions 1.8 and 9, that's probably okay.

No promises that this will make modern SWT work or anything, but it's
probably at least a beginnig toward that as well.

src/com/webcodepro/applecommander/storage/filters/imagehandlers/AppleImage.java
@@ -49,17 +49,17 @@ public abstract class AppleImage {
 	public static AppleImage create(int width, int height) {
 		String[] classes = {
 			"ImageIoImage", "SunJpegImage", "SwtImage" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		Class[] constructorArgClasses = new Class[] {
-			int.class, int.class };
+		Class<?> constructorArgType = AppleImage.class;
+		Class<?>[] constructorArgClasses = new Class[] { AppleImage.class };
 		Object[] constructorArgs = new Object[] {
 			new Integer(width), new Integer(height) };
 		for (int i=0; i<classes.length; i++) {
 			try {
-				Class appleImageClass = Class.forName(
+				Class<?> appleImageClass = Class.forName(
 					"com.webcodepro.applecommander.storage.filters.imagehandlers."  //$NON-NLS-1$
 					+ classes[i]);
-				Constructor constructor = 
-					appleImageClass.getConstructor(constructorArgClasses);
+				Constructor constructor =
+					constructorArgType.getConstructor(constructorArgClasses);
 				AppleImage appleImage = (AppleImage) 
 					constructor.newInstance(constructorArgs);
 				return appleImage;

build/build.xml
@@ -87,6 +87,7 @@
 		<javac srcdir="${source}" destdir="${classes}" classpath="${swtjar}">
 			<include name="**/*.java"/>
 			<exclude name="**/*Test.java"/>
+			<compilerarg value="-Xlint:unchecked"/>
 		</javac>
 		<jar jarfile="${guijar}" manifest="build/manifest.mf">
 			<fileset dir="${classes}"/>
@@ -107,6 +108,7 @@
 		<javac srcdir="${source}" destdir="${classes}" >
 			<include name="**/*.java"/>
 			<exclude name="**/*Test.java"/>
+			<compilerarg value="-Xlint:unchecked"/>
 			<compilerarg value="-XDignore.symbol.file"/>
 		</javac>
 		<jar jarfile="${cmdjar}" manifest="build/ac.mf">

src/com/webcodepro/applecommander/compiler/ApplesoftCompiler.java
@@ -72,25 +72,25 @@ public class ApplesoftCompiler implements ApplesoftTokens {
 	 * by the compiled program.  This map is keyed by the name of the
 	 * address and the value is the address.
 	 */
-	private Map knownAddresses = new HashMap();
+	private Map<String,String> knownAddresses = new HashMap<>();
 	/**
 	 * Lists the names of all addresses used by the compiled program.
 	 * To identify the value, use the knownAddresses map.
 	 */
-	private List usedAddresses = new ArrayList();
+	private List<String> usedAddresses = new ArrayList<>();
 	/**
 	 * Contains a list of all variables declared or used by the
 	 * program.
 	 */
-	private List variables = new ArrayList();
+	private List<Variable> variables = new ArrayList<>();
 	/**
 	 * Dynamically created map of commands to Methods.
 	 */
-	private Map commandMethods = new HashMap();
+	private Map<String,Method> commandMethods = new HashMap<>();
 	/**
 	 * Track FOR loop variables.
 	 */
-	private Stack loopVariables = new Stack();
+	private Stack<String> loopVariables = new Stack<>();
 	/**
 	 * Indicates integer math operations only.
 	 */

src/com/webcodepro/applecommander/storage/DirectoryEntry.java
@@ -37,7 +37,7 @@ public interface DirectoryEntry {
 	 * return value should always be a list - a directory
 	 * with 0 entries returns an empty list.
 	 */
-	public List getFiles();
+	public List<FileEntry> getFiles();
 
 	/**
 	 * Create a new FileEntry.

src/com/webcodepro/applecommander/storage/FileEntry.java
@@ -84,7 +84,7 @@ public interface FileEntry {
 	 * This default implementation is intended only for standard mode.
 	 * displayMode is specified in FormattedDisk.
 	 */
-	public List getFileColumnData(int displayMode);
+	public List<String> getFileColumnData(int displayMode);
 	
 	/**
 	 * Get file data.  This handles any operating-system specific issues.

src/com/webcodepro/applecommander/storage/FileEntryComparator.java
@@ -29,7 +29,7 @@ import java.util.Comparator;
  * Date created: Oct 27, 2002 8:24:39 PM
  * @author Rob Greene
  */
-public class FileEntryComparator implements Comparator {
+public class FileEntryComparator implements Comparator<FileEntry> {
 	private int columnIndex;
 	private int displayMode;
 	
@@ -45,11 +45,12 @@ public class FileEntryComparator implements Comparator {
 	 * Compare two FileEntry objects.
 	 * @see java.util.Comparator#compare(Object, Object)
 	 */
-	public int compare(Object o1, Object o2) {
+	public int compare(FileEntry o1, FileEntry o2) {
+		// FIXME?: This safety check is safe to remove now?
 		if (!(o1 instanceof FileEntry) || !(o2 instanceof FileEntry)) {
 			return 0;
 		}
-		
+
 		if (o1 == null || o2 == null) {
 			return ((o1 == null) ? -1 : 0) + ((o2 == null) ? 1 : 0);
 		}

src/com/webcodepro/applecommander/storage/FormattedDisk.java
@@ -185,8 +185,8 @@ public abstract class FormattedDisk extends Disk implements DirectoryEntry {
 	 * each disk format can build this as appropriate.  Each subclass should
 	 * override this method and add its own detail.
 	 */
-	public List getDiskInformation() {
-		List list = new ArrayList();
+	public List<DiskInformation> getDiskInformation() {
+		List<DiskInformation> list = new ArrayList<>();
 		list.add(new DiskInformation(textBundle.get("FormattedDisk.FileName"), getFilename())); //$NON-NLS-1$
 		list.add(new DiskInformation(textBundle.get("FormattedDisk.DiskName"), getDiskName())); //$NON-NLS-1$
 		list.add(new DiskInformation(textBundle.get("FormattedDisk.PhysicalSizeInBytes"), getPhysicalSize())); //$NON-NLS-1$
@@ -204,8 +204,8 @@ public abstract class FormattedDisk extends Disk implements DirectoryEntry {
 	 * Get the standard file column header information.
 	 * This default implementation is intended only for standard mode.
 	 */
-	public List getFileColumnHeaders(int displayMode) {
-		List list = new ArrayList();
+	public List<FileColumnHeader> getFileColumnHeaders(int displayMode) {
+		List<FileColumnHeader> list = new ArrayList<>();
 		list.add(new FileColumnHeader(textBundle
 				.get("Name"), 30, FileColumnHeader.ALIGN_LEFT)); //$NON-NLS-1$
 		list.add(new FileColumnHeader(textBundle

src/com/webcodepro/applecommander/storage/filters/GraphicsFileFilter.java
@@ -413,7 +413,7 @@ public class GraphicsFileFilter implements FileFilter {
 	 * @see <a href="http://web.pdx.edu/~heiss/technotes/ftyp/ft.about.html">File Types</a>
 	 */
 	public AppleImage[] buildQuickDraw2Icons(FileEntry fileEntry) {
-		List icons = new ArrayList();
+		List<AppleImage> icons = new ArrayList<>();
 		int offset = 26;	// skip file header
 		byte[] filedata = fileEntry.getFileData();
 		while (offset < filedata.length) {

commit c74e9ad3bd083b420bd1aea7177b0cfd8957f333

Fix my idiot attempt to fix this warning
My fix for the unchecked warning in AppleImage.java was completely dumb
and I had no idea what I was doing.  My understanding of how this works
is now greatly improved and the bug I introduced in 292afd2 is fixed.

src/com/webcodepro/applecommander/storage/filters/imagehandlers/AppleImage.java
@@ -49,8 +49,7 @@ public abstract class AppleImage {
 	public static AppleImage create(int width, int height) {
 		String[] classes = {
 			"ImageIoImage", "SunJpegImage", "SwtImage" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		Class<?> constructorArgType = AppleImage.class;
-		Class<?>[] constructorArgClasses = new Class[] { AppleImage.class };
+		Class[] constructorArgClasses = new Class[] { int.class, int.class };
 		Object[] constructorArgs = new Object[] {
 			new Integer(width), new Integer(height) };
 		for (int i=0; i<classes.length; i++) {
@@ -58,8 +57,9 @@ public abstract class AppleImage {
 				Class<?> appleImageClass = Class.forName(
 					"com.webcodepro.applecommander.storage.filters.imagehandlers."  //$NON-NLS-1$
 					+ classes[i]);
-				Constructor constructor =
-					constructorArgType.getConstructor(constructorArgClasses);
+				Constructor<?> constructor =
+					appleImageClass.getConstructor(constructorArgClasses);
+
 				AppleImage appleImage = (AppleImage) 
 					constructor.newInstance(constructorArgs);
 				return appleImage;