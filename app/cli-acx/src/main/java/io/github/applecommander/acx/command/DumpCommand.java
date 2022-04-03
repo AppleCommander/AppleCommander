d4f7d8afec1b44f4e6c464cf7629c4745a3af17b
Rob Greene <robgreene@gmail.com>
2022-04-02 22:26:36 -0500

    Dump command now allows offset into sectore/block.

--- app/cli-acx/src/main/java/io/github/applecommander/acx/command/DumpCommand.java
+++ app/cli-acx/src/main/java/io/github/applecommander/acx/command/DumpCommand.java
@@ -21,6 +21,7 @@ package io.github.applecommander.acx.command;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
@@ -28,35 +29,34 @@ import com.webcodepro.applecommander.util.AppleUtil;
 
 import io.github.applecommander.acx.arggroup.CoordinateSelection;
 import io.github.applecommander.acx.base.ReadOnlyDiskImageCommandOptions;
+import io.github.applecommander.acx.converter.IntegerTypeConverter;
 import io.github.applecommander.disassembler.api.Disassembler;
 import io.github.applecommander.disassembler.api.Instruction;
 import io.github.applecommander.disassembler.api.mos6502.InstructionSet6502;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
 import picocli.CommandLine.Option;
 
 @Command(name = "dump", description = "Dump a block or sector.")
 public class DumpCommand extends ReadOnlyDiskImageCommandOptions {
-    @ArgGroup(multiplicity = "1", heading = "%nCoordinate Selection:%n")
-    private CoordinateSelection coordinate = new CoordinateSelection();
-    
     @ArgGroup(heading = "%nOutput Selection:%n")
     private OutputSelection output = new OutputSelection();
-    
-    @Option(names = { "-a", "--address" }, description = "Starting address.")
-    private int address = 0x800;
+
+    @Mixin
+    private Options options = new Options();
 
     @Override
     public int handleCommand() throws Exception {
-        byte[] data = coordinate.read(disk);
-        System.out.println(output.format(address, data));
+        byte[] data = options.coordinate.read(disk);
+        System.out.println(output.format(options, data));
         return 0;
     }
     
     public static class OutputSelection {
-        private BiFunction<Integer,byte[],String> fn = this::formatHexDump;
-        public String format(int address, byte[] data) {
-            return fn.apply(address, data);
+        private BiFunction<Options,byte[],String> fn = this::formatHexDump;
+        public String format(Options options, byte[] data) {
+            return fn.apply(options, data);
         }
         
         @Option(names = "--hex", description = "Hex dump.")
@@ -69,13 +69,16 @@ public class DumpCommand extends ReadOnlyDiskImageCommandOptions {
             fn = this::formatDisassembly;
         }
         
-        public String formatHexDump(int address, byte[] data) {
-            return AppleUtil.getHexDump(address, data);
+        public String formatHexDump(Options options, byte[] data) {
+            return AppleUtil.getHexDump(options.address, data);
         }
         
-        public String formatDisassembly(int address, byte[] data) {
+        public String formatDisassembly(Options options, byte[] data) {
+            // If the offset is given, use that. If not, use 0 except for the boot sector and then use 1.
+            int calculatedOffset = options.offset.orElse(options.coordinate.includesBootSector() ? 1 : 0);
             return Disassembler.with(data)
-                    .startingAddress(address)
+                    .startingAddress(options.address)
+                    .bytesToSkip(calculatedOffset)
                     .use(InstructionSet6502.for6502())
                     .decode()
                     .stream()
@@ -99,5 +102,18 @@ public class DumpCommand extends ReadOnlyDiskImageCommandOptions {
             pw.printf(" %s\n", instruction.formatOperandWithValue());
             return sw.toString();
         }
+    }
+    
+    public static class Options {
+        @ArgGroup(multiplicity = "1", heading = "%nCoordinate Selection:%n")
+        private CoordinateSelection coordinate = new CoordinateSelection();
+        
+        @Option(names = { "-a", "--address" }, converter = IntegerTypeConverter.class, 
+                description = "Starting address.")
+        private int address = 0x800;
+        
+        @Option(names = { "-o", "--offset" }, converter = IntegerTypeConverter.class, 
+                description = "Number of bytes to skip into file before disassembling.")
+        private Optional<Integer> offset = Optional.empty();
     }
 }
