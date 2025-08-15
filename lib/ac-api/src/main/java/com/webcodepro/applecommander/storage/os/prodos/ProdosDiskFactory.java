87630ed81b78c50463fb4ba668d77f52858c8828
Rob Greene <robgreene@gmail.com>
2025-08-15 11:08:37 -0500

    Adding prototype for a ProDOS factory.

---
+++ lib/ac-api/src/main/java/com/webcodepro/applecommander/storage/os/prodos/ProdosDiskFactory.java
@@ -0,0 +1,62 @@
+package com.webcodepro.applecommander.storage.os.prodos;
+
+import com.webcodepro.applecommander.storage.Disk;
+import com.webcodepro.applecommander.storage.DiskFactory;
+import com.webcodepro.applecommander.storage.FormattedDisk;
+import com.webcodepro.applecommander.storage.physical.DosOrder;
+import com.webcodepro.applecommander.storage.physical.NibbleOrder;
+import com.webcodepro.applecommander.storage.physical.ProdosOrder;
+import org.applecommander.hint.Hint;
+import org.applecommander.util.DataBuffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdosDiskFactory implements DiskFactory {
+    @Override
+    public void inspect(Context ctx) {
+        // A Source should be removing any headers in the file, so we test against actual sizing:
+        boolean is140K = ctx.source.getSize() == Disk.APPLE_140KB_DISK;
+        boolean isNibble = ctx.source.getSize() == Disk.APPLE_140KB_NIBBLE_DISK;
+        boolean is400KPlus = ctx.source.getSize() > 50*16*256;     // Max DOS size
+        // It seems easiest to gather all possibilities first...
+        List<FormattedDisk> tests = new ArrayList<>();
+        if (ctx.source.is(Hint.NIBBLE_SECTOR_ORDER) || isNibble) {
+            tests.add(new ProdosFormatDisk(ctx.source.getName(), new NibbleOrder(ctx.source)));
+        }
+        else if (ctx.source.is(Hint.PRODOS_BLOCK_ORDER) || is400KPlus) {
+            tests.add(new ProdosFormatDisk(ctx.source.getName(), new ProdosOrder(ctx.source)));
+        }
+        else if (ctx.source.is(Hint.DOS_SECTOR_ORDER)) {
+            tests.add(new ProdosFormatDisk(ctx.source.getName(), new DosOrder(ctx.source)));
+        }
+        else if (is140K) {
+            // Could be either, so try both PO and DO
+            tests.add(new ProdosFormatDisk(ctx.source.getName(), new ProdosOrder(ctx.source)));
+            tests.add(new ProdosFormatDisk(ctx.source.getName(), new DosOrder(ctx.source)));
+        }
+        // ... and then test for ProDOS details:
+        for (FormattedDisk fdisk : tests) {
+            if (check(fdisk)) {
+                ctx.disks.add(fdisk);
+            }
+        }
+    }
+
+    public boolean check(FormattedDisk fdisk) {
+        DataBuffer volumeDirectory = DataBuffer.wrap(fdisk.readBlock(2));
+        // Check primary block for values
+        boolean good = volumeDirectory.getUnsignedShort(0x00) == 0              // prior block pointer is zero
+                    && (volumeDirectory.getUnsignedByte(0x04)&0xf0) == 0xf0     // storage_type = $f
+                    && volumeDirectory.getUnsignedByte(0x23) == 0x27            // entry_length = $27
+                    && volumeDirectory.getUnsignedByte(0x24) == 0x0d;           // entries_per_block = $d
+        // Now follow the directory blocks
+        while (good) {
+            int nextBlock = volumeDirectory.getUnsignedShort(0x02);
+            volumeDirectory = DataBuffer.wrap(fdisk.readBlock(nextBlock));
+            // Ensure that the prior link points to the block we just read
+            good = volumeDirectory.getUnsignedShort(0x00) == nextBlock;
+        }
+        return good;
+    }
+}
