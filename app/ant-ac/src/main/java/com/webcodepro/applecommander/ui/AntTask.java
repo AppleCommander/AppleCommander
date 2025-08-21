8ea0ce8875bd4cadeb6c4e43e56909502bb61335
Rob Greene <robgreene@gmail.com>
2025-08-21 13:23:54 -0500

    First draft trying a license check. Since I always forget it!

--- app/ant-ac/src/main/java/com/webcodepro/applecommander/ui/AntTask.java
+++ app/ant-ac/src/main/java/com/webcodepro/applecommander/ui/AntTask.java
@@ -1,9 +1,11 @@
 /*
- * ac - an AppleCommander command line utility
- * Copyright (C) 2002-2022 by Robert Greene
- * robgreene at users.sourceforge.net
- * Copyright (C) 2003-2022 by John B. Matthews
+ * AntTask - an AppleCommander Ant utility
+ * Copyright (C) 2012-2025 by David Schmidt
+ * david__schmidt at users.sourceforge.net
+ * Copyright (C) 2003-2025 by John B. Matthews
  * matthewsj at users.sourceforge.net
+ * Copyright (C) 2002-2025 by Robert Greene
+ * robgreene at users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the 
@@ -22,25 +24,6 @@
 package com.webcodepro.applecommander.ui;
 
 import java.io.FileInputStream;
-
-/*
- * Copyright (C) 2012 by David Schmidt
- * david__schmidt at users.sourceforge.net
- *
- * This program is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the 
- * Free Software Foundation; either version 2 of the License, or (at your 
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 import java.io.FileOutputStream;
 import java.io.IOException;
